6980ecb6496446563899d0f63aee86d6d8f05eed
nurlynnda <nurlynnda@graduate.utm.my>
2023-06-18 16:31:48 +0800

    carmovement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController _controller;
+    private Vector3 _playerVelocity;
+    private bool _groundedPlayer;
 
     [SerializeField]
     private float _playerSpeed = 5f;
@@ -15,26 +17,43 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Camera _followCamera;
 
-    private Vector3 _playerVelocity;
-    private bool _groundedPlayer;
+    [SerializeField]
+    private float _jumpHeight = 1f;
 
     [SerializeField]
-    private float _gravityValue = -9.81f;
+    private float _boostMultiplier = 2f;
 
-    private void Start() 
+    private float _gravityValue = -20f;
+
+    private void Start()
     {
-        _controller = GetComponent<CharacterController>();    
+        _controller = GetComponent<CharacterController>();
     }
 
-    private void Update() 
+    private void Update()
     {
-        Movement();    
+        Movement();
+
+        if (Input.GetButtonDown("Jump") && _groundedPlayer)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Boost();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            EndBoost();
+        }
     }
 
-    void Movement() 
+    private void Movement()
     {
         _groundedPlayer = _controller.isGrounded;
-        if (_groundedPlayer && _playerVelocity.y < 0) 
+        if (_groundedPlayer && _playerVelocity.y < 0)
         {
             _playerVelocity.y = 0f;
         }
@@ -45,18 +64,36 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movementInput = Quaternion.Euler(0, _followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
         Vector3 movementDirection = movementInput.normalized;
 
-        _controller.Move(movementDirection * _playerSpeed * Time.deltaTime);
+        float currentSpeed = _playerSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed *= _boostMultiplier;
+        }
 
-        if (movementDirection != Vector3.zero) 
+        _controller.Move(movementDirection * currentSpeed * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero)
         {
             Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-
             transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, _rotationSpeed * Time.deltaTime);
         }
 
         _playerVelocity.y += _gravityValue * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
+    }
 
+    private void Jump()
+    {
+        _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -2f * _gravityValue);
+    }
 
+    private void Boost()
+    {
+        _playerSpeed *= _boostMultiplier;
+    }
+
+    private void EndBoost()
+    {
+        _playerSpeed /= _boostMultiplier;
     }
 }
