c7f6286b1ace31301cd6e1dba6ecb0cd630e24ea
nurlynnda <nurlynnda@graduate.utm.my>
2023-07-07 02:54:27 +0800

    Game Levels
    Update Wall movements and player lives

---
+++ Assets/2. Scripts/LivesManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LivesManager : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed of the player's movement
+    public int startingLives = 3; // Starting number of lives
+    public TextMeshProUGUI livesText; // Text canvas to display remaining lives
+    public GameObject winningCanvas;
+    private int currentLives; // Current number of lives
+
+    private void Start()
+    {
+        currentLives = startingLives; // Set the current lives to the starting lives
+        UpdateLivesText(); // Update the text canvas with the initial number of lives
+    }
+
+    private void Update()
+    {
+        // Player movement logic goes here...
+
+        // Example: Move the player horizontally
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player collided with a wall
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Decrease the number of lives
+            currentLives--;
+
+            // Update the text canvas with the new number of lives
+            UpdateLivesText();
+
+            // Check if the player is out of lives
+            if (currentLives <= 0)
+            {
+                // Player is out of lives - perform game over logic
+                GameOver();
+            }
+        }
+    }
+
+    private void UpdateLivesText()
+    {
+        // Update the text canvas with the current number of lives
+        livesText.text = "Lives: " + currentLives.ToString();
+    }
+
+    private void GameOver()
+    {
+        winningCanvas.SetActive(true);
+    }
+}
--- Assets/2. Scripts/ObjectMovement.cs
+++ Assets/2. Scripts/ObjectMovement.cs
@@ -2,16 +2,31 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 5f;
+    public float moveSpeed = 5f; // Speed of the object's movement
+    public float leftDistance = 5f; // Distance the object should move to the left
+    public float rightDistance = 5f; // Distance the object should move to the right
+    private bool movingRight = true; // Flag to track the direction of movement
+    private Vector3 initialPosition; // Initial position of the object
 
     private void Start()
     {
-
+        // Store the initial position of the object
+        initialPosition = transform.position;
     }
 
-    void Update()
+    private void Update()
     {
-        transform.Translate(0, 0, 1 * speed * Time.deltaTime);
+        // Calculate the target position based on the direction
+        Vector3 targetPosition = movingRight ? initialPosition + new Vector3(rightDistance, 0f, 0f) : initialPosition - new Vector3(leftDistance, 0f, 0f);
+
+        // Move the object towards the target position
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        // Check if the object has reached the target position
+        if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+        {
+            // Reverse the direction when reaching the target position
+            movingRight = !movingRight;
+        }
     }
 }
