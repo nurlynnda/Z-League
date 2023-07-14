887679ee1788cec5a205935b0b4024eb6506fd27
nurlynnda <nurlynnda@graduate.utm.my>
2023-07-15 00:33:18 +0800

    Collectibles

---
+++ Assets/2. Scripts/Collectible.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
+
+        if (playerInventory != null)
+        {
+            playerInventory.DiamondCollected();
+            gameObject.SetActive(false);
+        }
+    }
+}
